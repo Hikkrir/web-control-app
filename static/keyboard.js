f34219ff461f3b6844ac109e09bf358d0c222491
gavrichenko nikita <gavrichenko.nikita@mail.ru>
2022-10-22 20:48:05 +0300

    feat:  теперь функция возвращает значения скорости и угла

--- static/keyboard.js
+++ static/keyboard.js
@@ -1,9 +1,42 @@
+let angle = 110
+let speed = 0
+
+window.addEventListener('keydown', (event) => {
+    if (event.repeat !== true){
+        switch(event.key){
+            case "s":
+                speed = -99
+                break;
+            case "w":
+                speed = 99
+                break;
+            case "a":
+                angle = 75
+                break;
+            case "d":
+                angle = 160
+                break;
+        }
+        fetch(`/keyboard_control/${angle}/${speed}`)
+    }
+})
+
 window.addEventListener('keyup', (event) => {
-    if (event.key === 's' 
-        || event.key === 'w'
-        || event.key === 'a'
-        || event.key === 'd'){
-        let key = event.key
-        fetch(`/keyboard_control/${key}`)
+    if (event.repeat !== true){
+        switch(event.key){
+            case "s":
+                speed = 0
+                break;
+            case "w":
+                speed = 0
+                break;
+            case "a":
+                angle = 110
+                break;
+            case "d":
+                angle = 110
+                break;
+            }
+            fetch(`/keyboard_control/${angle}/${speed}`)
     }
 })
