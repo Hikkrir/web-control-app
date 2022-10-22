10654d151c64fa6b0bd5c79df24fb15d12ccbc00
gavrichenko nikita <gavrichenko.nikita@mail.ru>
2022-10-22 22:03:00 +0300

    fix: исправлен баг с ненужным срабатыванием

--- static/keyboard.js
+++ static/keyboard.js
@@ -2,55 +2,59 @@ let angle = 110
 let speed = 0
 
 window.addEventListener('keydown', (event) => {
-    if (event.repeat !== true){
+    if (event.repeat === false){
         switch(event.key){
             case "a":
                 angle = 75
+                fetch(`/keyboard_control_angle/${angle}`)
                 break;
             case "d":
                 angle = 160
+                fetch(`/keyboard_control_angle/${angle}`)
                 break;
         }
-        fetch(`/keyboard_control_angle/${angle}`)
     }
 })
 window.addEventListener('keydown', (event) => {
-    if (event.repeat !== true){
+    if (event.repeat === false){
         switch(event.key){
             case "s":
                 speed = -99
+                fetch(`/keyboard_control_speed/${speed}`)
                 break;
             case "w":
                 speed = 99
+                fetch(`/keyboard_control_speed/${speed}`)
                 break;
         }
-        fetch(`/keyboard_control_speed/${speed}`)
     }
 })
 
 window.addEventListener('keyup', (event) => {
-    if (event.repeat !== true){
+    if (event.repeat === false){
         switch(event.key){
             case "s":
                 speed = 0
+                fetch(`/keyboard_control_speed/${speed}`)
                 break;
             case "w":
                 speed = 0
+                fetch(`/keyboard_control_speed/${speed}`)
                 break;
             }
-            fetch(`/keyboard_control_speed/${speed}`)
     }
 })
 window.addEventListener('keyup', (event) => {
-    if (event.repeat !== true){
+    if (event.repeat === false){
         switch(event.key){
             case "a":
                 angle = 110
+                fetch(`/keyboard_control_angle/${angle}`)
                 break;
             case "d":
                 angle = 110
+                fetch(`/keyboard_control_angle/${angle}`)
                 break;
             }
-            fetch(`/keyboard_control_angle/${angle}`)
     }
 })
