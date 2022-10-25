c098a5ac15d5b79f29329aaf52d1a20f4313da22
Gavrichenko Nikita <gavrichenko.nikita@mail.ru>
2022-10-25 17:17:22 +0300

    fix: ошибка с неправильным положением гистограммы

--- img_processing/road_detection.py
+++ img_processing/road_detection.py
@@ -20,7 +20,7 @@ class RoadDetection():
         self.TRACKBAR_HEIGHT_BOTTOM = 600
 
 
-        self.FRAME_WINDOW_WIDHT = 480
+        self.FRAME_WINDOW_WIDHT = 1280
         self.TRACKBAR_WINDOW_WIGTH = 360
         self.TRACKBAR_WINDOW_height = 360
 
