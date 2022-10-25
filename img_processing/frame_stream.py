14bdeac8d63dee1ed4846604b1ae478fb3d44f4e
Gavrichenko Nikita <gavrichenko.nikita@mail.ru>
2022-10-25 22:15:24 +0300

    hotfix

--- img_processing/frame_stream.py
+++ img_processing/frame_stream.py
@@ -2,5 +2,8 @@ import cv2 as cv
 cap = cv.VideoCapture(0, cv.CAP_DSHOW)
 
 def get_frame():
-    _, frame = cap.read()
-    return frame
+    ret, frame = cap.read()
+    if ret:
+        return frame
+    else:
+        return False
